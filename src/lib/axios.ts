f7f57d9f08052ade742084972e12de1e9a9a07e8
Mike Farrow <mike@farrow.io>
2025-09-14 15:31:44 +0100

    replacing proxy agent

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,14 +1,37 @@
 import axios from "axios";
-import { HttpsProxyAgent } from "hpagent";
 
-const httpsAgent = new HttpsProxyAgent({
-  proxy: `http://${import.meta.env.PROXY_USER}:${import.meta.env.PROXY_PASSWORD}@proxy.home.farrow.io:10080`,
-  rejectUnauthorized: false,
-  timeout: 20000,
-  proxyRequestOptions: {
+import {
+  type HttpsProxyAgentOptions,
+  HttpsProxyAgent,
+} from "https-proxy-agent";
+
+const extraOpts = Symbol("extra agent opts");
+
+export class PatchedHttpsProxyAgent<T> extends HttpsProxyAgent<T> {
+  private ca: any;
+
+  constructor(url: URL, opts: HttpsProxyAgentOptions<T>) {
+    super(url, opts);
+    (this as any)[extraOpts] = opts;
+  }
+
+  async connect(req: any, opts: any): Promise<any> {
+    return super.connect(
+      req,
+      Object.assign(req, { ...(this as any)[extraOpts], ...opts }),
+    );
+  }
+}
+
+const httpsAgent = new PatchedHttpsProxyAgent(
+  `https://${import.meta.env.PROXY_USER}:${import.meta.env.PROXY_PASSWORD}@proxy.home.farrow.io:10080`,
+  {
     rejectUnauthorized: false,
+    timeout: 20000,
+    sessionTimeout: 20000,
+    keepAlive: true,
   },
-});
+);
 
 export const client = axios.create({
   httpsAgent:
