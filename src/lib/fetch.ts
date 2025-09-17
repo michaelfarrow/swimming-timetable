5a6bc5ca470dcac754cedc0d4db7a0e47815cc53
Mike Farrow <mike@farrow.io>
2025-09-17 12:37:35 +0100

    adding retry

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,12 +1,18 @@
+import pRetry from "p-retry";
+
 export function fetchWithProxy(
   input: string | URL | Request,
   init?: RequestInit,
 ) {
-  return fetch(`http://proxy.home.farrow.io/proxy/${input}`, {
-    ...init,
-    headers: {
-      ...(init && init?.headers),
-      "X-Proxy-Token": import.meta.env.PROXY_PASSWORD,
-    },
-  });
+  return pRetry(
+    () =>
+      fetch(`http://proxy.home.farrow.io/proxy/${input}`, {
+        ...init,
+        headers: {
+          ...(init && init?.headers),
+          "X-Proxy-Token": import.meta.env.PROXY_PASSWORD,
+        },
+      }),
+    { retries: 5 },
+  );
 }
